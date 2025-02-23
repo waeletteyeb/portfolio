4052e41eceb588cd8ae453f2bd357312bfb9dbec
waeletteyeb <wael.etteyeb@enit-etudaint.utm.tn>
2025-02-23 20:43:37 +0100

    Update package-lock, package.json, and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
-
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { About, Contact, Experience, Social, Hero, Navbar, Tech, Works, StarsCanvas , Associative , Others  } from "./components";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router> {/* Change BrowserRouter to HashRouter */}
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -14,15 +13,15 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Associative/>
-        <Social/>
-        <Others/>
+        <Associative />
+        <Social />
+        <Others />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
-    </BrowserRouter>
+    </Router> 
   );
 }
 
